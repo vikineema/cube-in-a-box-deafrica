230d80380fba1863a8be1263eb5678ec685f857d
vikineema <victoria@kartoza.com>
2023-12-07 23:46:20 +0300

    Update docker installation bash script

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -2,17 +2,26 @@
 
 install_docker_ubuntu() {
   # Copied from : https://docs.docker.com/engine/install/ubuntu/
-  sudo apt-get remove -y docker docker-engine docker.io containerd runc
+  #sudo apt-get remove -y docker docker-engine docker.io containerd runc
+  for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
 
   sudo apt-get update
   sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  #echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   sudo apt-get update
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+  #sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
   sudo service docker start
 }
@@ -33,7 +42,8 @@ install_docker_fedora() {
 
   sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
-  sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-compose
+  #sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-compose
+  sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   sudo systemctl enable docker.service
   sudo systemctl enable containerd.service
   sudo systemctl start docker
